5dc323d81cec88f7be47cf91e11d658c3fc7d42a
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-19 00:01:32 +0900

    Delete PROJECT/OOP/W9 directory

--- PROJECT/OOP/W9/CircleArea/src/PrintSquares.java
+++
@@ -1,9 +0,0 @@
-public class PrintSquares {
-    public static void main(String[] args) {
-        int count = 1;
-        while (count <= 5) {
-            System.out.printf("%d squared is %d\n", count, count * count);
-            count++;
-        }
-    }
-}
