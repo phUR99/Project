26632017357dd72142e85d30c2731fa8657e9568
phUR99 <122969226+phUR99@users.noreply.github.com>
2024-12-19 00:08:21 +0900

    fixed

--- TEMP.cpp
+++ TEMP.cpp
@@ -4,19 +4,9 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL)
 #define ll long long
-void solve()
-{
-}
 
 int main()
 {
     fastio;
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
-
     return 0;
 }
