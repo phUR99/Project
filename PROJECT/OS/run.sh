ff7368409e19060c4ed460f2c6a9858214129712
phUR99 <skerrigen12@gmail.com>
2025-01-30 18:52:46 +0900

    1. run.sh, shell.c user.c, user.h, user.ld

--- PROJECT/OS/run.sh
+++ PROJECT/OS/run.sh
@@ -2,12 +2,19 @@ set -xue
 
 #QEMU 실행 파일 경로
 QEMU=qemu-system-riscv32
-
 CC=/opt/homebrew/opt/llvm/bin/clang
+OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
+
 CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
 
+
+
+$CC $CFLAGS -Wl, -Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
+$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
+$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
+
 $CC $CFLAGS -Wl, -Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c
+    kernel.c common.c shell.bin.o
 
 #QEMU 실행
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
