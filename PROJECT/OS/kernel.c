7980735e3e70b7a265921545a57642a57d1c933d
phUR99 <skerrigen12@gmail.com>
2025-01-30 19:35:36 +0900

    1. implement U-mode

--- PROJECT/OS/kernel.c
+++ PROJECT/OS/kernel.c
@@ -7,6 +7,7 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 extern char __free_ram[], __free_ram_end[];
 extern char __kernel_base[];
+extern char _binary_shell_bin_start[], _binary_shell_bin_size[];
 
 struct process procs[PROCS_MAX];
 struct process *proc_a;
@@ -54,8 +55,9 @@ void kernel_entry(void);
 void proc_a_entry(void);
 void proc_b_entry(void);
 void yield(void);
-struct process *create_process(uint32_t pc);
+struct process *create_process(const void *image, size_t image_size);
 void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags);
+void user_entry(void);
 
 void kernel_main(void)
 {
@@ -63,20 +65,15 @@ void kernel_main(void)
 	printf("\n\n");
 	WRITE_CSR(stvec, (uint32_t)kernel_entry);
 
-	idle_proc = create_process((uint32_t)NULL);
+	idle_proc = create_process(NULL, 0);
 	idle_proc->pid = -1;
 	current_proc = idle_proc;
 
-	proc_a = create_process((uint32_t)proc_a_entry);
-	proc_b = create_process((uint32_t)proc_b_entry);
-
+	create_process(_binary_shell_bin_start, (size_t)_binary_shell_bin_size);
+	
 	yield();
 	PANIC("switched to idle process");
 
-	for (;;)
-	{
-		__asm__ __volatile__("wfi");
-	}
 }
 
 __attribute((section(".text.boot")))
@@ -242,7 +239,7 @@ void proc_b_entry(void)
 	}
 }
 
-struct process *create_process(uint32_t pc)
+struct process *create_process(const void *image, size_t image_size)
 {
 	struct process *proc = NULL;
 	int i;
@@ -256,9 +253,7 @@ struct process *create_process(uint32_t pc)
 	}
 	if (!proc)
 		PANIC("no free process slots");
-	uint32_t *page_table = (uint32_t *)alloc_pages(1);
-	for (paddr_t paddr = (paddr_t)__kernel_base; paddr < (paddr_t)__free_ram_end; paddr += PAGE_SIZE)
-		map_page(page_table, paddr, paddr, PAGE_R  | PAGE_W | PAGE_X);
+
 	uint32_t *sp = (uint32_t *)&proc->stack[sizeof(proc->stack)];
 
 	*--sp = 0; // s11
@@ -273,7 +268,23 @@ struct process *create_process(uint32_t pc)
 	*--sp = 0; // s2
 	*--sp = 0; // s1
 	*--sp = 0; // s0
-	*--sp = (uint32_t)pc;
+	*--sp = (uint32_t)user_entry;
+
+	uint32_t *page_table = (uint32_t *)alloc_pages(1);
+
+	for (paddr_t paddr = (paddr_t)__kernel_base; paddr < (paddr_t)__free_ram_end; paddr += PAGE_SIZE)
+		map_page(page_table, paddr, paddr, PAGE_R | PAGE_W | PAGE_X);
+
+	for (uint32_t off = 0; off < image_size; off += PAGE_SIZE)
+	{
+		paddr_t page = alloc_pages(1);
+
+		size_t remaining = image_size - off;
+		size_t copy_size = PAGE_SIZE <= remaining ? PAGE_SIZE : remaining;
+
+		memcpy((void *)page, image + off, copy_size);
+		map_page(page_table, USER_BASE + off, page, PAGE_U | PAGE_R | PAGE_W | PAGE_X);
+	}
 
 	proc->pid = i + 1;
 	proc->state = PROC_RUNNABLE;
@@ -302,8 +313,8 @@ void yield(void)
 		"sfence.vma\n"
 		"csrw sscratch, %[sscratch]\n"
 		:
-		:	[satp] "r" (SATP_SV32 | ((uint32_t)next->page_table /PAGE_SIZE)),
-			[sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
+		: [satp] "r"(SATP_SV32 | ((uint32_t)next->page_table / PAGE_SIZE)),
+		  [sscratch] "r"((uint32_t)&next->stack[sizeof(next->stack)]));
 
 	struct process *prev = current_proc;
 	current_proc = next;
@@ -326,4 +337,16 @@ void map_page(uint32_t *table1, uint32_t vaddr, paddr_t paddr, uint32_t flags)
 	uint32_t vpn0 = (vaddr >> 12) & 0x3ff;
 	uint32_t *table0 = (uint32_t *)((table1[vpn1] >> 10) * PAGE_SIZE);
 	table0[vpn0] = ((paddr / PAGE_SIZE) << 10) | flags | PAGE_V;
+}
+__attribute__((naked))
+void user_entry(void)
+{
+	__asm__ __volatile__(
+		"csrw sepc, %[sepc]\n"
+		"csrw sstatus, %[sstatus]\n"
+		"sret\n"
+		:
+		:	[sepc] "r" (USER_BASE),
+			[sstatus] "r" (SSTATUS_SPIE)
+	);
 }
