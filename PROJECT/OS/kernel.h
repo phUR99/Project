d4bc17d0367ea39e2b1135347b73d785c261ded2
phUR99 <skerrigen12@gmail.com>
2025-01-30 15:47:59 +0900

    1. implement page table

--- PROJECT/OS/kernel.h
+++ PROJECT/OS/kernel.h
@@ -71,6 +71,13 @@ struct process
     int pid;
     int state;
     vaddr_t sp;
+    uint32_t *page_table;
     uint8_t stack[8192];
 };
 
+#define SATP_SV32 (1u << 31)
+#define PAGE_V (1<<0)
+#define PAGE_R (1<<1)
+#define PAGE_W (1<<2)
+#define PAGE_X (1<<3)
+#define PAGE_U (1<<4)
